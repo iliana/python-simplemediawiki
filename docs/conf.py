91ba36833f4c27895443d6fc25cc4256d08323d9
Ian Weller <ian@ianweller.org>
2013-02-14 11:41:01 -0600

    Update copyright year and add "and others"

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'simplemediawiki'
-copyright = '2011, Red Hat, Inc'
+copyright = '2013, Red Hat, Inc. and others'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
