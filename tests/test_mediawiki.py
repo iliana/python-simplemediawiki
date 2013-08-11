a4c17ddf325e9aec4739d54ce3f2c6f9e3ebae95
Ian Weller <ian@ianweller.org>
2013-08-11 12:49:40 -0400

    More test cases

--- tests/test_mediawiki.py
+++ tests/test_mediawiki.py
@@ -13,5 +13,21 @@ class MediaWikiTest(unittest.TestCase):
                                              '-ianweller.rhcloud.com/api.php'),
                                             user_agent=self.user_agent)
 
+    def test_call(self):
+        data = self.mw.call({'action': 'query',
+                             'meta': 'siteinfo'})
+        self.assertTrue('query' in data)
+        self.assertTrue('general' in data['query'])
+
+    def test_normalize_api_url_apidotphp(self):
+        self.assertTrue(self.mw.normalize_api_url() is not None)
+
+    def test_normalize_api_url_indexdotphp(self):
+        mw = simplemediawiki.MediaWiki(('https://simplemediawikitestsuite'
+                                        '-ianweller.rhcloud.com/index.php'),
+                                       user_agent=self.user_agent)
+        self.assertTrue(mw.normalize_api_url() is not None)
+        self.assertTrue('api.php' in mw._api_url)
+
     def test_login(self):
         self.assertTrue(self.mw.login('password', 'username'))
